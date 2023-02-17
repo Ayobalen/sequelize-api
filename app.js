40c7f9bb45d64fa5ef24f5aa973f1861b4fb0d25
opeshamz <olawoye51@gmail.com>
2023-02-17 11:36:23 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const { sequelize } = require('./models')
+const express = require('express')
 
-async function main(){
-    await sequelize.sync({ force : true })
-}
 
-main()
+const { sequelize, User } = require('./models')
+
+const app = express()
+app.use(express.json())
+
+app.post('/users', async(req, res) => {
+    const { name, email, role } = req.body
+
+    try{
+       const user = await User.create({ name, email, role})
+       return res.json(user)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+app.listen({ port: 5000 }, async() => {
+console.log("Server up on http://localhost:5000")
+await sequelize.sync({ force : true })
+console.log("Database synced")
+})
+   
